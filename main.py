891095a4530cf0b7722f8cfca558fe33bf0451ac
Pontus1337 <lindhpontus1@gmail.com>
2022-03-23 15:03:12 +0100

    Added a save function in the program

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ class Quiz:
                 print(f"You answered wrong :( , ur current score is  {self.score}")
             print()
 
-
-
     def get_score(self):
         return self.score
     
@@ -49,8 +47,39 @@ class Quiz:
 
     def quiz_start(self):
         print(f"This is the best quiz ever and is called {self.name}.\n")
-        input("Press enter to start\n")
-        
+        al =input("Press enter to start or write 1 to write your own questions\n")
+        if al =="1":
+            return al
+        else:
+            pass
+
+def make_question():
+    print("""Welcome to the question creator, first write down the question, then write
+    what which posistion the right answer will be and then write the diffrent alternatives.
+       """)
+    asks=input("The question: ")
+    placee=input("What place is the right answer?: ")
+    alternts=[]
+    u=""
+    while u=="":
+        analt=input("Write one of your alternatives here: ")
+        u=input("press enter to continue writing alternatives")
+        alternts.append(analt)
+    saves=Question(asks,placee,alternts)
+    savedalternaties=""
+    n=0
+    for w in saves.alternatives:
+        savedalternaties+=w
+        n+=1
+        if n <len(saves.alternatives):
+            savedalternaties+=","
+
+    save_string=f"{saves.question}/{saves.truanswer}/{savedalternaties}\n"
+    with open ("questions.txt","a", encoding="utf8") as f:
+        f.write(save_string)
+
+# In the text file "questions.txt" you need to write "Question"/"which place the right answer is for example 3"/ answer 1, answer2 2, answer 3,
+# Divide each question with a diffrent line    
 def load_questions():
     loaded=[]
     with open("questions.txt","r", encoding="utf8") as f:
@@ -60,16 +89,18 @@ def load_questions():
             loaded.append(l)
     return loaded
 
-# In the text file "questions.txt" you need to write "Question"/"which place the right answer is for example 3"/ answer 1, answer2 2, answer 3,
-# Divide each question with a diffrent line
+
 
         
 def main():
     quiz=Quiz("Quiz",[] ,0)
     quiz.questions.extend(load_questions())
     shuffle(quiz.questions)
-    quiz.quiz_start()
-    quiz.askquestions()
+    if quiz.quiz_start()=="1":
+        make_question()
+    else:
+        quiz.askquestions()
+    print()
     print(f"This is your end score: {quiz.get_score()}")
 
 
